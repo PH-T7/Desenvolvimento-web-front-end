62691d7d1bf5c20d3e71ec9d4051f7465fa28b50
PH <140414857+PH-T7@users.noreply.github.com>
2025-08-23 00:40:52 -0300

    adicionando atividades e typescript

---
+++ aula_1/TypeScript.js
@@ -0,0 +1,6 @@
+function somaJS(a, b) {
+    return a + b;
+}
+function somaTS(a, b) {
+    return a + b;
+}
---
+++ aula_1/TypeScript.ts
@@ -0,0 +1,9 @@
+function somaJS(a, b)
+{
+    return a + b;
+}
+
+function somaTS(a: number, b: number): number
+{
+    return a + b;
+}
